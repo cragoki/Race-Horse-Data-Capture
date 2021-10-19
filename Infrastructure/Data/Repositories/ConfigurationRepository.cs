686646b3d227d2f48d3c3f62b897e07654f81aed
Craig Rodger <craigrodger1@hotmail.com>
2021-10-19 18:04:40 +0000

    Adding logic to set the exact next execution, rather than letting this automatically update

--- Infrastructure/Data/Repositories/ConfigurationRepository.cs
+++ Infrastructure/Data/Repositories/ConfigurationRepository.cs
@@ -40,9 +40,19 @@ namespace Infrastructure.Data.Repositories
         public void UpdateNextExecution(JobEnum job)
         {
             var jobDb = _context.tb_job.Where(x => x.job_id == (int)job).FirstOrDefault();
-
+            var tomorrow = DateTime.Now.AddDays(1);
+            switch (job) 
+            {
+                case JobEnum.rhdcautomation:
+                    jobDb.next_execution = new DateTime(tomorrow.Year, tomorrow.Month, tomorrow.Day, 09, 00, 00);
+                    break;
+                case JobEnum.rhdcbacklog:
+                    break;
+                case JobEnum.rhdcresultretriever:
+                    jobDb.next_execution = new DateTime(tomorrow.Year, tomorrow.Month, tomorrow.Day, 22, 00, 00);
+                    break;
+            }
             jobDb.last_execution = DateTime.Now;
-            jobDb.next_execution = DateTime.Now.AddDays(1);
 
             _context.tb_job.Update(jobDb);
             _context.SaveChanges();
