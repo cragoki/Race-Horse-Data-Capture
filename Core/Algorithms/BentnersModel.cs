e259a2b9a0db79710669ef5f3f287a4f444ff77e
Craig Rodger <craigrodger1@hotmail.com>
2023-01-18 08:11:54 +0000

    BM - Setting up structure for new algorithm. Will now need to complete individual methods then link up to the application for automated predictions and manually run predictions

--- Core/Algorithms/BentnersModel.cs
+++ Core/Algorithms/BentnersModel.cs
@@ -29,7 +29,7 @@ namespace Core.Algorithms
             _algorithmRepository = algorithmRepository;
         }
 
-        public async Task<AlgorithmResult> GenerateAlgorithmResult(List<RaceEntity> races, List<AlgorithmVariableEntity> algorithms)
+        public async Task<AlgorithmResult> GenerateAlgorithmResult(List<RaceEntity> races)
         {
             var result = new AlgorithmResult();
             try
@@ -39,13 +39,13 @@ namespace Core.Algorithms
 
                 foreach (var race in races)
                 {
-                    var predictionResult = await FormCalculation(race, algorithms);
+                    var predictionResult = await RunModel(race);
 
-                    if (predictionResult != -1)
+                    if (predictionResult.Count() == 0)
                     {
                         Console.WriteLine($"Valid Prediction with {predictionResult}%");
                         raceCounter++;
-                        runningTotal.Add(predictionResult);
+                        runningTotal.Add(0);
                     }
                 }
 
@@ -64,31 +64,52 @@ namespace Core.Algorithms
             return result;
         }
 
-        public async Task<double> FormCalculation(RaceEntity race, List<AlgorithmVariableEntity> variables)
-        {
-            return 0;
-        }
-
-        public async Task<List<FormResultModel>> FormCalculationPredictions(RaceEntity race, List<AlgorithmSettingsEntity> settings, List<DistanceType> distances, List<GoingType> goings)
+        public async Task<List<FormResultModel>> RunModel(RaceEntity race)
         {
             var result = new List<FormResultModel>();
 
-            return result;
+            if (race.RaceHorses.All(x => x.position == 0))
+            {
+                return result;
+            }
+
+            //Variables
+            var settings = _configRepository.GetAlgorithmSettings((int)AlgorithmEnum.BentnersModel);
+            var placedRange = SharedCalculations.GetTake(race.no_of_horses ?? 0);
+            var counter = 0;
+            var horses = race.RaceHorses;
+            var listOfHorses = new List<HorseEntity>();
+            var distances = _mappingRepository.GetDistanceTypes();
+            var goings = _mappingRepository.GetGoingTypes();
+            var distanceGroups = VariableGroupings.GetDistanceGroupings(distances).Where(x => x.DistanceIds.Contains(race.distance ?? 0)).FirstOrDefault();
+            var goingGroups = VariableGroupings.GetGoingGroupings(goings).Where(x => x.ElementIds.Contains(race.going ?? 0)).FirstOrDefault();
+
+            //Settings
+
+
+            //BEGIN CALCULATING PREDICTED RESULTS
+            var horsepoints = new List<HorsePredictionModel>();
+            foreach (var horse in race.RaceHorses)
+            {
+                var toAdd = new HorsePredictionModel();
+                toAdd.points = 0;
+                toAdd.horse_id = horse.horse_id;
+                //Get past races for horse
+                var races = horse.Horse.Races.Where(x => x.Race.Event.created < race.Event.created).ToList();
+
+                if (races.Count() == 0)
+                {
+                    horsepoints.Add(toAdd);
+                    continue;
+                }
+
+                //Current Condition
+                toAdd.points += await GetCurrentCondition(race, horse.horse_id, settings);
+            }
+
+                return result;
         }
 
-        private async Task<string> CalculateHorsePredictability(int horseId, DateTime dateOfRace, RaceEntity race, GoingGroupModel goingGroup, DistanceGroupModel distanceGroup)
-        {
-            var result = 0M;
-
-            return result.ToString("0.00");
-        }
-
-        private async Task<List<FormResultModel>> AssignHorsePoints(List<AlgorithmSettingsEntity> settings, RaceEntity race, DistanceGroupModel distanceGroup, GoingGroupModel goingGroup, decimal distance)
-        {
-            var result = new List<FormResultModel>();
-
-            return result;
-        }
 
         /// <summary>
         /// Performance in last 2 races
@@ -96,9 +117,11 @@ namespace Core.Algorithms
         /// Age (TO ANALYSE OPTIMAL)
         /// </summary>
         /// <returns></returns>
-        private async Task<List<int>> GetCurrentCondition(RaceEntity race) 
+        private async Task<int> GetCurrentCondition(RaceEntity race, int horseId, List<AlgorithmSettingsEntity> settings) 
         {
-            var result = new List<int>();
+            var result = 0;
+            var reliabilityCurrentCondition = Decimal.Parse(settings.Where(x => x.setting_name == AlgorithmSettingEnum.reliabilityCurrentCondition.ToString()).FirstOrDefault().setting_value.ToString());
+
 
             return result;
         }
@@ -108,9 +131,10 @@ namespace Core.Algorithms
         /// Normalized times of Past races
         /// </summary>
         /// <returns></returns>
-        private async Task<List<int>> GetPastPerformance(RaceEntity race)
+        private async Task<int> GetPastPerformance(RaceEntity race, int horseId, List<AlgorithmSettingsEntity> settings)
         {
-            var result = new List<int>();
+            var result = 0;
+            var reliabilityPastPerformance = Decimal.Parse(settings.Where(x => x.setting_name == AlgorithmSettingEnum.reliabilityPastPerformance.ToString()).FirstOrDefault().setting_value.ToString());
 
             return result;
         }
@@ -121,9 +145,10 @@ namespace Core.Algorithms
         /// Jockeys Contribution to last races (performs well with this jockey? +1, performs well regardless of jockey? +1, has not placed with this jockey? 0)
         /// </summary>
         /// <returns></returns>
-        private async Task<List<int>> GetAdjustmentsPastPerformance(RaceEntity race)
+        private async Task<int> GetAdjustmentsPastPerformance(RaceEntity race, int horseId, List<AlgorithmSettingsEntity> settings)
         {
-            var result = new List<int>();
+            var result = 0;
+            var reliabilityAdjustmentsPastPerformance = Decimal.Parse(settings.Where(x => x.setting_name == AlgorithmSettingEnum.reliabilityAdjustmentsPastPerformance.ToString()).FirstOrDefault().setting_value.ToString());
 
             return result;
         }
@@ -133,9 +158,10 @@ namespace Core.Algorithms
         /// Todays Jockeys ability
         /// </summary>
         /// <returns></returns>
-        private async Task<List<int>> GetPresentRaceFactors(RaceEntity race)
+        private async Task<int> GetPresentRaceFactors(RaceEntity race, int horseId, List<AlgorithmSettingsEntity> settings)
         {
-            var result = new List<int>();
+            var result = 0;
+            var reliabilityPresentRaceFactors = Decimal.Parse(settings.Where(x => x.setting_name == AlgorithmSettingEnum.reliabilityPresentRaceFactors.ToString()).FirstOrDefault().setting_value.ToString());
 
             return result;
         }
@@ -147,9 +173,10 @@ namespace Core.Algorithms
         /// Specific track preference
         /// </summary>
         /// <returns></returns>
-        private async Task<List<int>> GetHorsePreferences(RaceEntity race)
+        private async Task<int> GetHorsePreferences(RaceEntity race, int horseId, List<AlgorithmSettingsEntity> settings)
         {
-            var result = new List<int>();
+            var result = 0;
+            var reliabilityHorsePreferences = Decimal.Parse(settings.Where(x => x.setting_name == AlgorithmSettingEnum.reliabilityHorsePreferences.ToString()).FirstOrDefault().setting_value.ToString());
 
             return result;
         }
--- Core/Interfaces/Algorithms/IBentnersModel.cs
+++ Core/Interfaces/Algorithms/IBentnersModel.cs
@@ -8,8 +8,7 @@ namespace Core.Interfaces.Algorithms
 {
     public interface IBentnersModel
     {
-        Task<double> FormCalculation(RaceEntity race, List<AlgorithmVariableEntity> variables);
-        Task<List<FormResultModel>> FormCalculationPredictions(RaceEntity race, List<AlgorithmSettingsEntity> settings, List<DistanceType> distances, List<GoingType> goings);
-        Task<AlgorithmResult> GenerateAlgorithmResult(List<RaceEntity> races, List<AlgorithmVariableEntity> algorithms);
+        Task<List<FormResultModel>> RunModel(RaceEntity race);
+        Task<AlgorithmResult> GenerateAlgorithmResult(List<RaceEntity> races);
     }
 }
